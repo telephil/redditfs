f1d11ed5fd70d19dcc41f1a723100473cd317e9c
Philippe Mechai <philippe.mechai@gmail.com>
2015-01-07 21:34:20 +0100

    start porting code to plan9 libc

--- redditfs.c
+++ redditfs.c
@@ -1,21 +1,22 @@
-#include <stdio.h>
+#include <u.h>
+#include <libc.h>
 #include "reddit_client.h"
 
-int
+void
 main(int argc, const char* argv[])
 {
 	const char* sub = argc == 2 ? argv[1] : "plan9";
 	struct rc_error error;
 	struct rc_post** posts = rc_get(sub, &error);
 	if(posts == NULL) {
-		fprintf(stderr, "error: %s\n", error.message);
-		return 1;
+		fprint(2, "error: %s\n", error.message);
+		exits(error.message);
 	}
 	for(int i = 0; posts[i] != NULL; i++) {
-		printf("[%ld] %s\n => %s\n", 
+		print("[%ld] %s\n => %s\n", 
 			posts[i]->score,
 			posts[i]->title,
 			posts[i]->url);
 	}
-	return 0;
+	exits(0);
 }
